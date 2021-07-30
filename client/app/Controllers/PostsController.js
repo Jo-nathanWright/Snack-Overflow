5280e34392e5de2fe9a79a58c1e6cf7b6c1e4055
Jonathan Wright <j.wright72127@gmail.com>
2021-07-30 17:56:01 -0600

    Now Pulls comments and post on page load

--- client/app/Controllers/PostsController.js
+++ client/app/Controllers/PostsController.js
@@ -1,4 +1,15 @@
-import { ProxyState } from '../AppState'
+import { postsService } from '../Services/PostsService.js'
 
 export default class PostController {
+  constructor() {
+    this.getAll()
+  }
+
+  async getAll() {
+    try {
+      await postsService.getAll()
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
--- client/app/main.js
+++ client/app/main.js
@@ -1,9 +1,11 @@
 import { AuthController } from './Controllers/AuthController.js'
 import CommentsController from './Controllers/CommentsController.js'
+import PostController from './Controllers/PostsController.js'
 
 class App {
   authController = new AuthController();
-  commmentsController = new CommentsController()
+  commmentsController = new CommentsController();
+  postsController = new PostController();
 }
 
 window.app = new App()
