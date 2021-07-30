482c7483ce87c54c770423051d5d96e28a0d8ca6
Jonathan Wright <j.wright72127@gmail.com>
2021-07-30 11:37:23 -0600

    Post Controller, and Index footer

---
+++ server/services/PostsService.js
@@ -0,0 +1,25 @@
+import { BadRequest } from '../utils/Errors'
+
+class PostsService {
+  getAll(query) {
+    throw new Error('Method not implemented.')
+  }
+
+  getById(id) {
+    throw new Error('Method not implemented.')
+  }
+
+  create(body) {
+    throw new Error('Method not implemented.')
+  }
+
+  edit(body) {
+    throw new Error('Method not implemented.')
+  }
+
+  destroy(id) {
+    throw new Error('Method not implemented.')
+  }
+}
+
+export const postsService = new PostsService()
