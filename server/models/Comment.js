62abbbfec1f32f45fd98a1ad7319dbb41265c624
Jonathan Wright <j.wright72127@gmail.com>
2021-07-30 10:24:15 -0600

    models made

---
+++ server/models/Comment.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+export const Comment = new Schema(
+  {
+    imgUrl: { type: String },
+    description: { type: String, required: true },
+    creatorId: { type: ObjectId, required: true },
+    postId: { type: ObjectId, ref: 'Post', required: true }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+)
