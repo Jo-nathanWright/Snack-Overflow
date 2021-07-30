40af59225d2b1ef78707e41f0a58fc83aa12a2cc
Timothy Kimble <timble417@gmail.com>
2021-07-30 10:28:06 -0600

    initializing

--- client/app/env.js
+++
@@ -1,5 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'codeworks-jonathan.us.auth0.com'
-export const audience = 'https://SnackOverflow.com'
-export const clientId = 'mdr2TM5XboiEZnfgjh7pVvCbcLekffpW'
